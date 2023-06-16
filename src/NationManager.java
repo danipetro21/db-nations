bc2242d710637a3cb4fd7d228c0feacbe12ad5eb
danipetro21 <idany346@gmail.com>
2023-06-16 14:48:58 +0200

    milestone 3 upload

--- src/NationManager.java
+++ src/NationManager.java
@@ -1,21 +1,28 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NationManager {
 
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
         String url = "jdbc:mysql://localhost:8889/db_nations";
         String user = "root";
         String password = "root";
 
         ArrayList<Nation> listaNazioni = new ArrayList<>();
 
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("inserisci un input di ricerca: ");
+        String in = scan.nextLine();
+
         try (Connection con = DriverManager.getConnection(url, user, password)) {
             String query = "SELECT c.name, c.country_id , r.name, c2.name \n" +
                     "FROM countries c \n" +
                     "JOIN regions r ON r.region_id = c.region_id \n" +
                     "JOIN continents c2 ON c2.continent_id = r.continent_id \n" +
+                    "WHERE c.name LIKE '%" + in + "%'\n" +
                     "ORDER BY c.name ";
 
             try (PreparedStatement ps = con.prepareStatement(query)) {
@@ -33,6 +40,7 @@ public class NationManager {
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
+            System.out.println("nonp osdoa");
         }
 
         for (int i = 0; i < listaNazioni.size(); i++) {
