3b43701ebb2018e097c87f9a1c9b2c9c38065563
danipetro21 <idany346@gmail.com>
2023-06-16 14:36:10 +0200

    milestone 2 upload

--- src/Nation.java
+++ src/Nation.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Nation {
+public class Nation {
+
+    private String nameNation;
+    private int idNation;
+    private String nameRegion;
+    private String nameContinent;
+
+
+    public Nation(String nameNation, int idNation, String nameRegion, String nameContinent) {
+        this.nameNation = nameNation;
+        this.idNation = idNation;
+        this.nameRegion = nameRegion;
+        this.nameContinent = nameContinent;
+    }
+
+    public String getNameNation() {
+        return nameNation;
+    }
+
+    public void setNameNation(String nameNation) {
+        this.nameNation = nameNation;
+    }
+
+    public int getIdNation() {
+        return idNation;
+    }
+
+    public void setIdNation(int idNation) {
+        this.idNation = idNation;
+    }
+
+    public String getNameRegion() {
+        return nameRegion;
+    }
+
+    public void setNameRegion(String nameRegion) {
+        this.nameRegion = nameRegion;
+    }
+
+    public String getNameContinent() {
+        return nameContinent;
+    }
+
+    public void setNameContinent(String nameContinent) {
+        this.nameContinent = nameContinent;
+    }
+
+    @Override
+    public String toString() {
+        return "Nation{" +
+                "nameNation='" + nameNation + '\'' +
+                ", idNation=" + idNation +
+                ", nameRegion='" + nameRegion + '\'' +
+                ", nameContinent='" + nameContinent + '\'' +
+                '}';
+    }
 }
--- src/NationManager.java
+++ src/NationManager.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class NationManager {
+import java.sql.*;
+import java.util.ArrayList;
+
+public class NationManager {
+
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:8889/db_nations";
+        String user = "root";
+        String password = "root";
+
+        ArrayList<Nation> listaNazioni = new ArrayList<>();
+
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
+            String query = "SELECT c.name, c.country_id , r.name, c2.name \n" +
+                    "FROM countries c \n" +
+                    "JOIN regions r ON r.region_id = c.region_id \n" +
+                    "JOIN continents c2 ON c2.continent_id = r.continent_id \n" +
+                    "ORDER BY c.name ";
+
+            try (PreparedStatement ps = con.prepareStatement(query)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        String nameNation = rs.getString(1);
+                        int idNation = rs.getInt(2);
+                        String nameRegion = rs.getString(3);
+                        String nameContinent = rs.getString(4);
+
+                        Nation nazione = new Nation(nameNation, idNation, nameRegion, nameContinent);
+                        listaNazioni.add(nazione);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        for (int i = 0; i < listaNazioni.size(); i++) {
+            System.out.println(listaNazioni.get(i).toString());
+        }
+    }
 }
